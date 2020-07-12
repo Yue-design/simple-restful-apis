0bebc891b0e3f8c4346e75c87baad7758e10956e
yue <yue.liu@authright.com>
2020-07-12 13:55:52 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  // res.send("Welcome to Yue's first node API!");
+  res.send("Welcome to Yue's first nodemon API!");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
